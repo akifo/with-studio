2c51a4202e27c7f621b020a2415c117d87f91b70
akifo <akiho@bb07.net>
2021-11-07 16:42:06 +0900

    build

--- dist/content.js
+++ dist/content.js
@@ -1,13 +1,21 @@
 const generatorWhiteList = {
   STUDIO: true,
   Webflow: true,
+  'Wix.com Website Builder': true,
 };
 
 (function () {
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const nodeList = document.getElementsByName('generator');
-  if (!nodeList) return;
-  if (!nodeList[0]) return;
-  const generator = nodeList[0].content;
-  const message = generatorWhiteList[generator] ? generator : 'others';
-  chrome.runtime.sendMessage(message);
+  let message = 'others';
+  if (nodeList && nodeList[0]) {
+    const generator = nodeList[0].content;
+    if (generatorWhiteList[generator]) message = generator;
+    chrome.runtime.sendMessage({ message, isDark });
+  } else {
+    const targetElement = document.getElementById('shopify-features');
+    const isShopify = typeof targetElement !== 'undefined';
+    if (isShopify) message = 'Shopify';
+    chrome.runtime.sendMessage({ message, isDark });
+  }
 })();
